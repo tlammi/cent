b980ad0889c3c2470a0673a3fda0bdcb47df3ebf
tlammi <tangojulietlima@gmail.com>
2022-09-03 12:55:36 +0300

    Utility for hiding exceptions
    This allows for a possible noexcept build at some point

---
+++ lib/include/cent/raise.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <sstream>
+#include <stdexcept>
+
+namespace cent {
+namespace detail {
+
+template <class T, class... Ts>
+void accumulate_ss(std::stringstream& ss, T&& t, Ts&&... ts) {
+    ss << std::forward<T>(t);
+    if constexpr (sizeof...(Ts)) { accumulate_ss(ss, std::forward<Ts>(ts)...); }
+}
+}  // namespace detail
+
+template <class... Ts>
+[[noreturn]] void raise(Ts&&... ts) {
+    std::stringstream ss;
+    detail::accumulate_ss(ss, std::forward<Ts>(ts)...);
+    throw std::runtime_error(ss.str());
+}
+}  // namespace cent
