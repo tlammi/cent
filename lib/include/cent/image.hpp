cb0f682d028632bb49183a88b926a764bdc7fa60
tlammi <tangojulietlima@gmail.com>
2022-09-03 09:46:03 +0300

    clang-format

--- lib/include/cent/image.hpp
+++ lib/include/cent/image.hpp
@@ -18,59 +18,66 @@ namespace cent {
  */
 class Image {
  public:
-  Image(std::string str);
-  Image& operator=(std::string str);
+    /**
+     * Image from a reference
+     *
+     * \param str E.g. "docker.io/ubuntu:20.40"
+     */
+    Image(std::string str);
 
-  Image(const Image&) = default;
-  Image& operator=(const Image&) = default;
+    /// Assign a new value to the str
+    Image& operator=(std::string str);
 
-  Image(Image&&) noexcept = default;
-  Image& operator=(Image&&) noexcept = default;
+    Image(const Image&) = default;
+    Image& operator=(const Image&) = default;
 
-  /**
-   * Access the tag in the image
-   *
-   * E.g.
-   * - "foo:bar" -> "bar"
-   * - "foo" -> "latest"
-   */
-  std::string_view tag() const noexcept;
+    Image(Image&&) noexcept = default;
+    Image& operator=(Image&&) noexcept = default;
 
-  /**
-   * Return the unmodified string.
-   *
-   * This is the same value stored in the constructor.
-   */
-  const std::string& str() const noexcept;
+    /**
+     * Access the tag in the image
+     *
+     * E.g.
+     * - "foo:bar" -> "bar"
+     * - "foo" -> "latest"
+     */
+    std::string_view tag() const noexcept;
 
-  /**
-   * Get the registry name from the image
-   *
-   * Parses up to the first '/' and returns that. If no registry is stored in
-   * the image, an empty string is returned.
-   * E.g.
-   * - "foo" -> ""
-   * - "localhost/bar" -> "localhost"
-   * - "localhost:5000/bar" -> "localhost:5000"
-   */
-  std::string_view registry() const noexcept;
+    /**
+     * Return the unmodified string.
+     *
+     * This is the same value stored in the object e.g. via constructor.
+     */
+    const std::string& str() const noexcept;
 
-  /**
-   * Image name without registry and tag parts
-   *
-   * Note that this expects a domain to be present.
-   */
-  std::string_view name() const noexcept;
+    /**
+     * Get the registry name from the image
+     *
+     * Parses up to the first '/' and returns that. If no registry is stored in
+     * the image, an empty string is returned.
+     * E.g.
+     * - "foo" -> ""
+     * - "localhost/bar" -> "localhost"
+     * - "localhost:5000/bar" -> "localhost:5000"
+     */
+    std::string_view registry() const noexcept;
 
-  /**
-   * Everything except the tag
-   */
-  std::string_view repo() const noexcept;
+    /**
+     * Image name without registry and tag parts
+     *
+     * Note that this expects a domain to be present.
+     */
+    std::string_view name() const noexcept;
+
+    /**
+     * Everything except the tag
+     */
+    std::string_view repo() const noexcept;
 
  private:
-  static constexpr inline auto NPOS = std::string::npos;
-  size_t tag_separator() const noexcept;
+    static constexpr inline auto NPOS = std::string::npos;
+    size_t tag_separator() const noexcept;
 
-  std::string m_str;
+    std::string m_str;
 };
 }  // namespace cent
