f37695e3c88682ead74caaca53fc2a6943725a93
tlammi <tangojulietlima@gmail.com>
2022-09-08 21:22:04 +0300

    Switch to CPR for no-rtti

--- lib/src/curl_http_session.cpp
+++ lib/src/curl_http_session.cpp
@@ -1,6 +1,5 @@
-#include <curlpp/Easy.hpp>
-#include <curlpp/Infos.hpp>
-#include <curlpp/Options.hpp>
+
+#include <cpr/cpr.h>
 
 #include "cent/http/header.hpp"
 #include "cent/http_session.hpp"
@@ -11,8 +10,8 @@ namespace cent {
 class CurlHttpSession final : public HttpSession {
  public:
     CurlHttpSession() {
-        m_easy.setOpt(curlpp::options::Verbose(true));
-        m_easy.setOpt(curlpp::options::FollowLocation(true));
+        m_sess.SetVerbose(true);
+        m_sess.SetRedirect(cpr::Redirect(true));
     }
 
     void on_header(std::string_view field, std::string& value) override {
@@ -25,54 +24,57 @@ class CurlHttpSession final : public HttpSession {
     }
     void set_header_field(std::string_view field,
                           std::string_view value) override {
-        std::stringstream ss{};
-        ss << field << ": " << value;
-        auto iter = m_headers.begin();
-        const auto end = m_headers.end();
-        while (iter != end) {
-            http::HeaderView header{*iter};
-            if (header.field_name() == field) {
-                *iter = ss.str();
-                return;
-            }
-            ++iter;
-        }
-        m_headers.push_back(ss.str());
+        m_headers[std::string(field)] = std::string(value);
     }
+
     virtual std::string_view get_body() override {
         return m_body.rdbuf()->view();
     }
 
     int get(std::string_view url) override {
         m_body = std::stringstream();
-        m_easy.setOpt(curlpp::options::HttpHeader(m_headers));
-        m_easy.setOpt(curlpp::options::Url(std::string(url)));
-        m_easy.setOpt(curlpp::options::HeaderFunction([&](char* buf, size_t s,
-                                                          size_t n) {
-            if (auto header =
-                    http::HeaderView::try_parse(std::string_view(buf, s * n))) {
+        m_sess.SetOption(m_headers);
+        m_sess.SetUrl(url);
+        m_sess.SetHeaderCallback(cpr::HeaderCallback([&](std::string buf,
+                                                         intptr_t /*unused*/) {
+            if (auto header = http::HeaderView::try_parse(buf)) {
                 auto iter = m_on_header.find(std::string(header->field_name()));
                 if (iter != m_on_header.end()) {
                     iter->second.get() = header->value();
                 }
             }
-            return s * n;
+            return true;
         }));
 
-        m_easy.setOpt(
-            curlpp::options::WriteFunction([&](char* buf, size_t s, size_t n) {
-                m_body << std::string_view(buf, s * n);
-                return s * n;
+        m_sess.SetWriteCallback(
+            cpr::WriteCallback([&](std::string buf, intptr_t /*unused*/) {
+                m_body << buf;
+                return true;
             }));
+        auto resp = m_sess.Get();
+        return resp.status_code;
+
+        // m_easy.setOpt(curlpp::options::HttpHeader(m_headers));
+        // m_easy.setOpt(curlpp::options::Url(std::string(url)));
+        /*m_easy.setOpt(curlpp::options::HeaderFunction(
+            [&](char* buf, size_t s, size_t n) { return s * n; }));
+*/
+        /*
+                m_easy.setOpt(
+                    curlpp::options::WriteFunction([&](char* buf, size_t s,
+           size_t n) { m_body << std::string_view(buf, s * n); return s * n;
+                    }));
+
         m_easy.perform();
         return curlpp::infos::ResponseCode::get(m_easy);
+                    */
     }
 
  private:
-    curlpp::Easy m_easy{};
+    cpr::Session m_sess{};
     std::map<std::string, std::reference_wrapper<std::string>, std::less<>>
         m_on_header{};
-    std::list<std::string> m_headers{};
+    cpr::Header m_headers{};
     std::stringstream m_body{};
 };
 
