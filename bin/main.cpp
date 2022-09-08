96a1b5b396609defb5aba699d9496629fed1731f
tlammi <tangojulietlima@gmail.com>
2022-09-08 22:06:24 +0300

    Minimal CLI

--- bin/main.cpp
+++ bin/main.cpp
@@ -7,8 +7,17 @@
 #include "interface.hpp"
 int main(int argc, char** argv) {
     CLI::App app{"Container Engine"};
+    app.require_subcommand();
     CLI::App* pull_cmd = app.add_subcommand("pull", "Pull an image");
+    std::string image;
+    pull_cmd->add_option("image", image);
+    pull_cmd->final_callback([&]() {
+        Interface i{};
+        cent::Cent c{&i};
+        std::cerr << c.pull(image) << '\n';
+    });
     CLI::App* run_cmd = app.add_subcommand("run", "Run a container");
+    run_cmd->final_callback([]() { std::cerr << "you wanted to run\n"; });
     CLI11_PARSE(app, argc, argv);
 
     /*
