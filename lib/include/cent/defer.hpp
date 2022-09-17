71a63d9bc61f809a1a415ea6eaf70d148ee050d1
tlammi <tangojulietlima@gmail.com>
2022-09-17 17:22:00 +0300

    Defer type

---
+++ lib/include/cent/defer.hpp
@@ -0,0 +1,35 @@
+
+#pragma once
+
+#include <concepts>
+#include <utility>
+
+namespace cent {
+
+template <class F = std::nullptr_t>
+requires(std::invocable<F> || std::same_as<F, std::nullptr_t>) class Defer {
+ public:
+    template <class F2>
+    Defer(F2&& f) : m_f{std::forward<F>(f)} {}
+
+    Defer(const Defer&) = delete;
+    Defer& operator=(const Defer&) = delete;
+
+    Defer(Defer&&) = delete;
+    Defer& operator=(Defer&&) = delete;
+
+    ~Defer() { m_f(); }
+
+ private:
+    F m_f;
+};
+
+/// No-op defer
+template <>
+class Defer<std::nullptr_t> {};
+
+template <class T>
+Defer(T&&) -> Defer<T>;
+Defer()->Defer<std::nullptr_t>;
+
+}  // namespace cent
