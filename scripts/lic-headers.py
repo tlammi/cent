f3755b457024f3acdfaafbea2282fd77ba23020f
tlammi <tangojulietlima@gmail.com>
2022-09-18 10:19:57 +0300

    Copyright support

--- scripts/lic-headers.py
+++ scripts/lic-headers.py
@@ -3,11 +3,13 @@
 import sys
 import os
 import hashlib
+import datetime
 
 import util
 
 SPDX_HEADER_FIELD = "SPDX-License-Identifier"
 SPDX_HEADER = f"/* {SPDX_HEADER_FIELD}:  GPL-3.0-or-later */".strip()
+COPYRIGHT_HEADER = f"/* Copyright (C) {datetime.datetime.now().year} Toni Lammi */"
 
 EXPECTED_LICENSE_DIGEST = "3972dc9744f6499f0f9b2dbf76696f2ae7ad8af9b23dde66d6af86c9dfb36986"
 
@@ -33,33 +35,45 @@ def first_line(path: str):
 
 
 def check(srcs: list):
-    failed=[]
+    missing_spdx=[]
+    missing_copyright=[]
     for s in srcs:
-        line = first_line(s)
-        if line.strip() != SPDX_HEADER:
-            failed.append(s)
+        with open(s, "r") as f:
+            lines = f.readlines()
+        if lines[0].strip() != SPDX_HEADER:
+            missing_spdx.append(s)
+        else:
+            lines = lines[1:]
+        if "Copyright" not in lines[0]:
+            missing_copyright.append(s)
 
-    if len(failed) > 0:
+    result = 0
+    if len(missing_spdx) > 0:
         print("# Missing license header for")
-        print("\n".join(failed))
-        return 1
-    return 0
+        print("\n".join(missing_spdx))
+        result = 1
+    if len(missing_copyright) > 0:
+        print("# Missing copyright header for")
+        print("\n".join(missing_copyright))
+        result = 1
+    return result
 
 
 def inject(srcs: list):
     for s in srcs:
-        line = first_line(s)
-        if line.strip() != SPDX_HEADER:
-            with open(s, "r") as f:
-                lines = f.readlines()
-            if SPDX_HEADER_FIELD not in line:
+        with open(s, "r") as f:
+            lines = f.readlines()
+        if lines[0].strip() != SPDX_HEADER:
+            if SPDX_HEADER_FIELD not in lines[0]:
                 # No SPDX header ->  Prepend
                 lines = [f"{SPDX_HEADER}\n"] + lines
             else:
                 # SPDX header present -> Replace
                 lines = [f"{SPDX_HEADER}\n"] + lines[1:]
-            with open(s, "w") as f:
-                f.writelines(lines)
+        if "Copyright" not in lines[1]:
+            lines = [lines[0]] + [f"{COPYRIGHT_HEADER}\n"] + lines[1:]
+        with open(s, "w") as f:
+            f.writelines(lines)
 
 
 def main():
