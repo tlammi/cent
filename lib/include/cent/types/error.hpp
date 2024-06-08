27a9b0a3a4ae42a9d91b3c3b5ed2bd21a8c1cbe6
tlammi <tangojulietlima@gmail.com>
2024-06-08 10:43:49 +0300

    sdfasdf

--- lib/include/cent/types/error.hpp
+++ lib/include/cent/types/error.hpp
@@ -11,9 +11,18 @@ using ErrorCode = error_detail::Errno;
 
 class Error {
  public:
-    constexpr explicit Error(int e, str::StaticStr msg = "")
-        : m_code(ErrorCode{e}),
-          m_msg(msg.empty() ? error_detail::errno_to_desc(m_code) : msg) {}
+    constexpr Error(int e)
+        : m_code(ErrorCode(e)), m_msg(error_detail::errno_to_desc(m_code)) {}
+
+    template <size_t S>
+    consteval Error(int e, const char (&arr)[S]) noexcept
+        : Error(e, str::StaticStr(arr)) {}
+
+    constexpr explicit Error(int e, str::StaticStr msg) noexcept
+        : m_code(ErrorCode{e}), m_msg(msg) {}
+
+    constexpr explicit Error(int e, const std::string& msg)
+        : m_code(ErrorCode{e}), m_msg(msg) {}
 
     constexpr ErrorCode code() const noexcept { return m_code; }
     constexpr str::CStr message() const noexcept { return m_msg.view(); }
--- test/unit/types/error.cpp
+++ test/unit/types/error.cpp
@@ -12,3 +12,19 @@ TEST(Error, CustomMsg) {
     ASSERT_EQ(e.code(), cent::ErrorCode::Perm);
     ASSERT_EQ(e.message(), "Custom");
 }
+
+TEST(Error, Constexpr) {
+    static constexpr auto e = cent::Error(E2BIG, "asdf");
+    ASSERT_EQ(e.code(), cent::ErrorCode::Toobig);
+    ASSERT_EQ(e.message(), "asdf");
+}
+
+TEST(Error, DynAlloc) {
+    auto create = []() {
+        auto msg = std::string("foo") + "bar" + "baz";
+        return cent::Error(EPERM, msg);
+    };
+    auto e = create();
+    ASSERT_EQ(e.code(), cent::ErrorCode::Perm);
+    ASSERT_EQ(e.message(), "foobarbaz");
+}
