1380f02a0e679e21a2fc379ac8443d101f926543
Toni Lammi <tangojulietlima@gmail.com>
2024-09-16 18:24:26 +0300

    EnumArr

--- lib/include/cent/bits/containers/enum_arr.hpp
+++ lib/include/cent/bits/containers/enum_arr.hpp
@@ -15,11 +15,52 @@ namespace cent {
 template <concepts::enum_type E, class T>
 class EnumArr {
  public:
+    using enum_type = E;
+    using value_type = T;
+    constexpr EnumArr() noexcept = default;
+
+    template <class... Ts>
+    explicit constexpr EnumArr(std::in_place_type_t<T> /*unused*/, Ts&&... ts)
+        : m_arr{std::forward<Ts>(ts)...} {}
+
     constexpr size_t size() const noexcept { return m_arr.size(); }
 
+    template <class Self>
+    constexpr auto& operator[](this Self&& self, E e) noexcept {
+        const auto idx = to_index(e);
+        return std::forward<Self>(self).m_arr[idx];
+    }
+
+    template <class Self>
+    constexpr auto& at(this Self&& self, E e) {
+        const auto idx = to_idx(e);
+        return std::forward<Self>(self).m_arr.at(idx);
+    }
+
  private:
+    static constexpr size_t to_index(E e) noexcept {
+        return *magic_enum::enum_index(e);
+    }
+
     std::array<T, magic_enum::enum_count<E>()> m_arr{};
 };
+
+namespace enum_arr_detail {
+template <concepts::enum_type E, class T, class F, class... Ts>
+constexpr void make_enum_arr(EnumArr<E, T>& a, E e, F&& f, Ts&&... ts) {
+    a[e] = std::forward<F>(f);
+    if constexpr (sizeof...(Ts)) { make_enum_arr(a, std::forward<Ts>(ts)...); }
+}
+
+}  // namespace enum_arr_detail
+
+template <concepts::enum_type E, class T, class... Ts>
+constexpr auto make_enum_arr(Ts&&... ts) {
+    EnumArr<E, T> arr{};
+    if constexpr (sizeof...(Ts))
+        enum_arr_detail::make_enum_arr(arr, std::forward<Ts>(ts)...);
+    return arr;
+}
 
 }  // namespace cent
 
--- lib/test/unit/containers/enum_arr.cpp
+++ lib/test/unit/containers/enum_arr.cpp
@@ -1,3 +1,5 @@
+#include "cent/bits/containers/enum_arr.hpp"
+
 #include <gtest/gtest.h>
 
 #include <cent/containers.hpp>
@@ -17,6 +19,13 @@ enum class Gaps {
     D = 10,
 };
 
+enum class Mixed {
+    A = 3,
+    B = -1,
+    C = 4,
+    D = 2,
+};
+
 TEST(Ctor, Empty) {
     cent::EnumArr<Empty, int> a{};
     ASSERT_EQ(a.size(), 0);
@@ -30,5 +39,33 @@ TEST(Ctor, Sequential) {
 TEST(Ctor, Gaps) {
     cent::EnumArr<Sequence, int> a{};
     ASSERT_EQ(a.size(), 4);
+}
+
+TEST(Index, Sequence) {
+    cent::EnumArr<Sequence, int> a{std::in_place_type<int>, 1, 2, 3, 4};
+    ASSERT_EQ(a[Sequence::A], 1);
+    ASSERT_EQ(a[Sequence::B], 2);
+}
+
+TEST(Index, Gaps) {
+    cent::EnumArr<Gaps, int> a{std::in_place_type<int>, 1, 2, 3, 4};
+    ASSERT_EQ(a[Gaps::A], 1);
+    ASSERT_EQ(a[Gaps::B], 2);
+    ASSERT_EQ(a[Gaps::C], 3);
+    ASSERT_EQ(a[Gaps::D], 4);
+}
+
+TEST(Index, Mixed) {
+    cent::EnumArr<Mixed, int> a{std::in_place_type<int>, 1, 2, 3, 4};
+    ASSERT_EQ(a[Mixed::B], 1);
+    ASSERT_EQ(a[Mixed::D], 2);
+    ASSERT_EQ(a[Mixed::A], 3);
+    ASSERT_EQ(a[Mixed::C], 4);
+}
+
+TEST(MakeArr, Empty) {
+    auto arr = cent::make_enum_arr<Mixed, int>();
+    static_assert(
+        std::same_as<std::remove_cvref_t<decltype(arr)>::enum_type, Mixed>);
 }
 
