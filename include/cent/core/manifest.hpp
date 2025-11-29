f842045b653e997f845df799f76aa74f833600d3
Toni Lammi <tangojulietlima@gmail.com>
2025-11-29 09:01:22 +0200

    start writing a better manifest

--- include/cent/core/manifest.hpp
+++ include/cent/core/manifest.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <map>
+#include <memory>
 #include <string>
 #include <vector>
 
@@ -20,6 +21,33 @@ struct Manifest {
     std::vector<std::string> layers{};
     // Image annotations
     std::map<std::string, std::string> annotations{};
+};
+
+namespace manifest_detail {
+class Impl;
+}
+
+class Manifest2 {
+ public:
+    explicit Manifest2(std::string v);
+    explicit Manifest2(std::vector<std::byte> v);
+    Manifest2(Manifest2&) = delete;
+    Manifest2& operator=(Manifest2&) = delete;
+
+    Manifest2(Manifest2&&) noexcept = default;
+    Manifest2& operator=(Manifest2&&) noexcept = default;
+
+    ~Manifest2();
+
+    std::string_view config_digest();
+    std::vector<std::string_view> layer_digests();
+
+    std::vector<std::pair<std::string_view, std::string_view>> annotations();
+
+    std::string_view data() noexcept;
+
+ private:
+    std::unique_ptr<manifest_detail::Impl> m_impl;
 };
 
 }  // namespace cent
