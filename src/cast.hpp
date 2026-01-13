02b09e664dcd0c11643c2cc23d1424b10efb79cf
Toni Lammi <tangojulietlima@gmail.com>
2026-01-13 19:32:41 +0200

    asdfasdf

--- src/cast.hpp
+++ src/cast.hpp
@@ -1,15 +1,3 @@
 #pragma once
 
-#include "concepts.hpp"
-
-namespace cent {
-template <enum_type E>
-constexpr auto underlying_cast(E e) noexcept {
-    return static_cast<std::underlying_type_t<E>>(e);
-}
-
-template <enum_type E>
-constexpr auto underlying_cast(std::underlying_type_t<E> t) noexcept {
-    return static_cast<E>(t);
-}
-}  // namespace cent
+#include "cent/cast.hpp"
